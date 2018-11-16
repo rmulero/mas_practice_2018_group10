fd00156f1b0bf57ce4a29cf2fa47195eceeff84c
DESKTOP-NV58J34\Rafael Mulero <rafamuve@gmail.com>
2018-11-16 02:37:11 +0100

    SearcherAgent modified to store the autonomy, charging points and its position in the map

--- src/cat/urv/imas/agent/SearcherAgent.java
+++ src/cat/urv/imas/agent/SearcherAgent.java
@@ -1,12 +1,23 @@
 package cat.urv.imas.agent;
 
 import cat.urv.imas.behaviour.SetupBehaviour;
+import cat.urv.imas.map.Cell;
+import cat.urv.imas.map.CellType;
+import cat.urv.imas.ontology.GameSettings;
 import jade.core.AID;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearcherAgent extends ImasAgentTuned {
     
+    private int maxSteps = 0;
+    private int currentStep = 0;
+    private Cell position;
+    private final List<Cell> chargingPoints;
+    
     public SearcherAgent() {
         super( AgentType.SEARCHER );
+        this.chargingPoints = new ArrayList<>();
     }
     
     @Override
@@ -29,5 +40,25 @@ public class SearcherAgent extends ImasAgentTuned {
     @Override
     protected void takeDown() {
         deRegisterDF();
+    }
+    
+    @Override
+    public void onSettingsReceived(GameSettings gameSettings) {
+        // get autonomy
+        this.maxSteps = gameSettings.geteSearcherMaxSteps();
+        
+        // get charging points
+        List<Cell> recycle = gameSettings.getCellsOfType().get( CellType.BATTERIES_CHARGE_POINT );
+        this.chargingPoints.addAll( recycle );
+        
+        // get agent position
+        List<Cell> agents = gameSettings.getAgentList().get( this.type );
+        String localName = getLocalName();
+        localName = localName.replace("seag", "");
+        int index = Integer.valueOf( localName );
+        this.position = agents.get( index-1 );
+        
+        log( "Position [" + position.getRow() + "," + position.getCol() + "]");
+        log( "Autonomy (" + maxSteps + ")" );
     }
 }
