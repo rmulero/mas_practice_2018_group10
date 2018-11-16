c39b5111942376071a745ef84905e4d50d5f09a9
DESKTOP-NV58J34\Rafael Mulero <rafamuve@gmail.com>
2018-11-16 02:37:41 +0100

    CleanerAgent modified to store the capacity, recycling points and its position in the map

--- src/cat/urv/imas/agent/CleanerAgent.java
+++ src/cat/urv/imas/agent/CleanerAgent.java
@@ -1,12 +1,23 @@
 package cat.urv.imas.agent;
 
 import cat.urv.imas.behaviour.SetupBehaviour;
+import cat.urv.imas.map.Cell;
+import cat.urv.imas.map.CellType;
+import cat.urv.imas.ontology.GameSettings;
 import jade.core.AID;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CleanerAgent extends ImasAgentTuned {
     
+    private int maxCapacity = 0;
+    private int currentLoad = 0;
+    private Cell position;
+    private final List<Cell> recyclingPoints; 
+    
     public CleanerAgent() {
         super( AgentType.CLEANER );
+        this.recyclingPoints = new ArrayList<>();
     }
     
     @Override
@@ -29,5 +40,25 @@ public class CleanerAgent extends ImasAgentTuned {
     @Override
     protected void takeDown() {
         deRegisterDF();
+    }
+    
+    @Override
+    public void onSettingsReceived(GameSettings gameSettings) {
+        // get capacity
+        this.maxCapacity = gameSettings.getCleanerCapacity();
+        
+        // get recycling points
+        List<Cell> recycle = gameSettings.getCellsOfType().get( CellType.RECYCLING_POINT_CENTER );
+        this.recyclingPoints.addAll( recycle );
+        
+        // get agent position
+        List<Cell> agents = gameSettings.getAgentList().get( this.type );
+        String localName = getLocalName();
+        localName = localName.replace("clag", "");
+        int index = Integer.valueOf( localName );
+        this.position = agents.get( index-1 );
+        
+        log( "Position [" + position.getRow() + "," + position.getCol() + "]");
+        log( "Capacity (" + maxCapacity  + ")" );
     }
 }
