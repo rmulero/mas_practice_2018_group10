19f78823a8b05208a4ef0ef8648071228bc68047
DESKTOP-NV58J34\Rafael Mulero <rafamuve@gmail.com>
2018-12-10 13:07:33 +0100

    Behaviours added. Behaviours to handle request and responses created in separated classes.

---
+++ src/cat/urv/imas/behaviour/coordinator/CoordinatorRequestActionsBehaviour.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.urv.imas.behaviour.coordinator;
+
+import cat.urv.imas.agent.ImasAgentTuned;
+import jade.core.AID;
+import jade.lang.acl.ACLMessage;
+import jade.proto.AchieveREInitiator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ *
+ * @author Rafael Mulero
+ */
+public class CoordinatorRequestActionsBehaviour extends AchieveREInitiator {
+    
+    private final ImasAgentTuned imasAgent;
+    private final ACLMessage request;
+    
+    public CoordinatorRequestActionsBehaviour(ImasAgentTuned a, ACLMessage msg, ACLMessage request) {
+        super(a, msg);
+        this.imasAgent = a;
+        this.request = request;
+    }
+    
+    @Override
+    protected void handleAgree( ACLMessage agree ) {
+        imasAgent.log( "AGREE received from " + ((AID) agree.getSender()).getLocalName() );
+    }
+
+    @Override
+    protected void handleRefuse( ACLMessage refuse ) {
+        imasAgent.log( "REFUSE received from " + ((AID) refuse.getSender()).getLocalName() );
+    }
+
+    @Override
+    protected void handleInform( ACLMessage inform ) {
+        imasAgent.log( "INFORM received from " + ((AID) inform.getSender()).getLocalName() );
+    }
+
+    @Override
+    protected void handleFailure( ACLMessage failure ) {
+        imasAgent.log( "FAILURE received from " + ((AID) failure.getSender()).getLocalName() );
+    }
+    
+    @Override
+    protected void handleAllResultNotifications( Vector resultNotifications ) {
+        List<String> actions = new ArrayList<>();
+        for ( Object notification : resultNotifications ){
+            ACLMessage response = (ACLMessage) notification;
+            String content = response.getContent();
+            actions.add( content );
+        }
+        
+        String actionsStr = String.join( ",", actions );
+        imasAgent.onActionsReceived( request, actionsStr );
+    }
+}
---
+++ src/cat/urv/imas/behaviour/system/SystemRequestUpdatesBehaviour.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.urv.imas.behaviour.system;
+
+import cat.urv.imas.agent.ImasAgentTuned;
+import jade.core.AID;
+import jade.lang.acl.ACLMessage;
+import jade.proto.AchieveREInitiator;
+
+/**
+ *
+ * @author Rafael Mulero
+ */
+public class SystemRequestUpdatesBehaviour extends AchieveREInitiator {
+
+    private ImasAgentTuned imasAgent;
+
+    public SystemRequestUpdatesBehaviour(ImasAgentTuned a, ACLMessage msg) {
+        super(a, msg);
+        this.imasAgent = a;
+    }
+
+    @Override
+    protected void handleAgree(ACLMessage agree) {
+        imasAgent.log("AGREE received from " + ((AID) agree.getSender()).getLocalName());
+    }
+
+    @Override
+    protected void handleRefuse(ACLMessage refuse) {
+        imasAgent.log("REFUSE received from " + ((AID) refuse.getSender()).getLocalName());
+    }
+
+    @Override
+    protected void handleInform(ACLMessage inform) {
+        imasAgent.log("INFORM received from " + ((AID) inform.getSender()).getLocalName());
+        imasAgent.onUpdateConfirmed();
+    }
+
+    @Override
+    protected void handleFailure(ACLMessage failure) {
+        imasAgent.log("FAILURE received from " + ((AID) failure.getSender()).getLocalName());
+    }
+
+}
