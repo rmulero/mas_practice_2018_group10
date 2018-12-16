d442bc856451ef7accd66c76dbe31cb9d7bcdbad
DESKTOP-NV58J34\Rafael Mulero <rafamuve@gmail.com>
2018-12-16 02:12:32 +0100

    New behaviours added. Behaviours to treat the actions / updates in the searcher / cleaner agents.

---
+++ src/cat/urv/imas/behaviour/agent/AgentResponseUpdatesBehaviour.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.urv.imas.behaviour.coordinator;
+
+import cat.urv.imas.agent.ImasAgentTuned;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREResponder;
+
+/**
+ *
+ * @author Rafael Mulero
+ */
+public class CoordinatorResponseUpdatesBehaviour extends AchieveREResponder {
+    
+    private final ImasAgentTuned imasAgent;
+    
+    public CoordinatorResponseUpdatesBehaviour( ImasAgentTuned a, MessageTemplate template){
+        super(a, template);
+        this.imasAgent = a;
+    }
+
+    @Override
+    protected ACLMessage handleRequest(ACLMessage request) throws NotUnderstoodException, RefuseException {
+        imasAgent.log( "REQUEST received from " + request.getSender().getName() );
+
+        imasAgent.onUpdateRequest( request );
+
+        imasAgent.log( "Sending AGREE" );
+        ACLMessage agree = request.createReply();
+        agree.setPerformative(ACLMessage.AGREE);
+        return agree;
+    }
+}
