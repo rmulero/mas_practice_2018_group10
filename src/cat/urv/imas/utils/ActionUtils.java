23922d73919df55b4700fe608e112b97db114c11
DESKTOP-NV58J34\Rafael Mulero <rafamuve@gmail.com>
2018-12-16 02:26:00 +0100

    ActionUtils class added. Class containing strings to be used for the actions.

---
+++ src/cat/urv/imas/utils/ActionUtils.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.urv.imas.utils;
+
+/**
+ *
+ * @author Rafael Mulero
+ */
+public class ActionUtils {
+    
+    public static final String MOVE_ACTION = "move";
+    public static final String DETECT_ACTION = "detect";
+    public static final String NEED_CHARGE_FLAG = "need_charge";
+    
+    public static final String ACTION_PART_DELIMITER = ":";
+    public static final String ACTION_DELIMITER = ";";
+}
