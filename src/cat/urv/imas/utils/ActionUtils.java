e7e2edb3ba9704f3dc2abe295e53fa20e9269485
DESKTOP-NV58J34\Rafael Mulero <rafamuve@gmail.com>
2019-01-02 17:03:38 +0100

    ActinUtils modified. Added Strings for the rest of the actions

--- src/cat/urv/imas/utils/ActionUtils.java
+++ src/cat/urv/imas/utils/ActionUtils.java
@@ -13,7 +13,9 @@ public class ActionUtils {
     
     public static final String ACTION_MOVE = "move";
     public static final String ACTION_DETECT = "detect";
-    public static final String ACTION_CHANGE_DIRECTION = "change_direction";
+    public static final String ACTION_PROPOSE = "propose";
+    public static final String ACTION_COLLECT = "collect";
+    public static final String ACTION_RECYCLE = "recycle";
     
     public static final String FLAG_NEED_CHARGE = "need_charge";
     
