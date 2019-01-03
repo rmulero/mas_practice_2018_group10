172e90185d4b7d8cdb78834d76f850135a0707a3
DESKTOP-NV58J34\Rafael Mulero <rafamuve@gmail.com>
2019-01-03 04:09:35 +0100

    ActionUtils modified. Added actions to distinguish between waste collection start and end.

--- src/cat/urv/imas/utils/ActionUtils.java
+++ src/cat/urv/imas/utils/ActionUtils.java
@@ -14,7 +14,8 @@ public class ActionUtils {
     public static final String ACTION_MOVE = "move";
     public static final String ACTION_DETECT = "detect";
     public static final String ACTION_PROPOSE = "propose";
-    public static final String ACTION_COLLECT = "collect";
+    public static final String ACTION_COLLECT_START = "collect_start";
+    public static final String ACTION_COLLECT_END = "collect_end";
     public static final String ACTION_RECYCLE = "recycle";
     
     public static final String FLAG_NEED_CHARGE = "need_charge";
